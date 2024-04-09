fca11a8347e56f44d73f73b2c603ef6411cc0e5a
Gloria Villa <hello.gloria@gmail.com>
2024-04-09 10:38:50 -0700

    made first component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Product from './Product'
+
+function App() {
+  return (
+    <>
+    <h1>Hello There</h1>
+    <Product />
+    </>
+  )
+}
+
+export default App
---
+++ src/Product.jsx
@@ -0,0 +1,7 @@
+function Product(){
+    return(
+        <h1>I am a product file</h1>
+    )
+}
+
+export default Product;
