08c3e017d5c2c59306f684c1a9bc3cbb959b726b
Gloria Villa <hello.gloria@gmail.com>
2024-04-15 01:29:45 -0700

    add useState for user

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
 
   
 
+
   return (
     <>
       <h1>Main Component</h1>
--- src/User.jsx
+++ src/User.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import { useState } from 'react';
 
-function User()
-{    
+function User() {
+
+    let [name, setName] = useState("Saurabh");
+
+    function changeName() {
+        setName("Thor");
+        console.log("new name is " + name);
+    }
     return (
         <div className="user">
-            <h1>Saurabh</h1>
+            <h1>{name}</h1>
+            <button onClick={changeName}>Change Name</button>
         </div>
+
     )
 }
 
