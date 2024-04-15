737f6246d66b58043ba31134e0e8b3c294d13d36
Gloria Villa <hello.gloria@gmail.com>
2024-04-15 08:40:25 -0700

    add anonymous function to pass a variable with onClick

--- src/User.jsx
+++ src/User.jsx
@@ -9,10 +9,28 @@ function User() {
         setName("Thor");
         console.log("new name is " + name);
     }
+
+    function doSomething(planet) {
+        
+        console.log(planet);
+    }
+
+
     return (
         <div className="user">
-            <h1>{name}</h1>
+            <h1>User Component</h1>
+            <h2>{name}</h2>
             <button onClick={changeName}>Change Name</button>
+            {/* You need to use an anonymous function to pass an argument or the function will be called immediately and not update the state */}
+            <button onClick={function(){
+                doSomething("Earth");
+            }}>Do Something</button>
+            {/* You can also use arrow function 
+            <button onClick={()=>{
+                doSomething("Earth");
+            }}>Do Something</button>
+            */}
+
         </div>
 
     )
